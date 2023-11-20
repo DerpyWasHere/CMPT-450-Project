cd30eb2864b22eb2a385decbfaa165c5717dc8bb
Sean Chan <seanchan@telus.net>
2023-11-20 15:42:05 +0000

    Perceptron: 	Added private class variables to be instantiated by constructor 	Added constructor to public class functions 	Added param include from generated header from BranchPredictor.py

--- src/cpu/pred/perceptron.hh
+++ src/cpu/pred/perceptron.hh
@@ -1,10 +1,29 @@
 #include "base/types.hh"
 #include "cpu/pred/bpred_unit.hh"
+#include "params/Perceptron.hh"
+
+#define NUMBER_OF_WEIGHTS 10 
+#define GHL 32 
+#define THRESHOLD 128
+#define WL 20 
 
 class PerceptronBP : public BPredUnit
 {
+    private:
+        bool *GHR;
+        int8_t WT[1<<NUMBER_OF_WEIGHTS][WL];
+        uint8_t threshold = 0; // Dynamic threshold to compare aganist hard coded max in define 
+
+        uint32_t num_of_weights;
+        uint32_t ghl;
+        uint32_t wl;
+
+        uint64_t weight_hash(Addr pc, uint32_t wt_size);
+        bool predict();
 
     public:
+        PerceptronBP(const PerceptronBPParams *params);
+
         bool lookup(ThreadID tid, Addr branch_addr, void* &bp_history) override;
         void uncondBranch(ThreadID tid, Addr br_pc, void* &bp_history) override;
         void btbUpdate(ThreadID tid, Addr branch_addr, void* &bp_history) override;
