eab8ccd81d0b37ac4b61088fbf27f8d43f6767ce
Sean Chan <seanchan@telus.net>
2023-11-20 14:00:42 +0000

    Added easy slurm build script

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+#SBATCH --cpus-per-task=8
+#SBATCH --time=60:00
+#SBATCH --mem=1G
+
+srun scons -j 8 build/RISCV/gem5.opt CPU_MODELS='AtomicSimpleCPU,O3CPU,TimingSimpleCPU,MinorCPU' --gold-linker
