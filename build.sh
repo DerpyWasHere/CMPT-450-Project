8333a9c1319a00bf902b09b3dad4e949772ee227
Sean Chan <seanchan@telus.net>
2023-11-27 06:56:52 +0000

    build.sh: 	Changed build target to X86 from RISCV

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 #SBATCH --time=60:00
 #SBATCH --mem=1G
 
-srun scons -j 8 build/RISCV/gem5.opt CPU_MODELS='AtomicSimpleCPU,O3CPU,TimingSimpleCPU,MinorCPU' --gold-linker
+srun scons -j 8 build/X86/gem5.opt CPU_MODELS='AtomicSimpleCPU,O3CPU,TimingSimpleCPU,MinorCPU' --gold-linker
