14dcc84def4e95803d8cb53d1502830eb3d895bd
Alen Mehmedbegovic <ama225@sfu.ca>
2023-11-28 03:02:02 +0000

    Debugging seg fault attempts2

--- SimpleInfBenchmark.sh
+++ SimpleInfBenchmark.sh
@@ -4,4 +4,4 @@
 #SBATCH --time=60:00
 #SBATCH --mem=32G
 
-srun build/X86/gem5.opt -re --outdir=$PWD/results/X86/run_micro/CCa/Simple/Inf --debug-flags=ExecAll --debug-file=trace.out --debug-start=0 --debug-end=100000 gem5-config/run_micro.py Simple Inf microbenchmark/CCa/bench.X86
+srun build/X86/gem5.opt -re --outdir=$PWD/results/X86/run_micro/CCa/Simple/Inf --debug-flags=Event,ExecAll --debug-file=trace.out --debug-start=0 --debug-break=1000 gem5-config/run_micro.py Simple Inf microbenchmark/CCa/bench.X86
